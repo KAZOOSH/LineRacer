db9ab2b9fbff18ebe51d98238a8d26f796ab0ba4
Brian Eschrich <hi@brinoausrino.com>
2019-05-15 20:31:36 +0200

    arduino updates

--- Arduino_sketches/bike_zeitabstand/bike_zeitabstand.ino
+++ Arduino_sketches/bike_zeitabstand/bike_zeitabstand.ino
@@ -6,9 +6,10 @@ const int lInputs = 3;
 
 long lastSequence[2] = {0,0};
 int tSequence = 500;
+int tMax = 1500;
 bool lastInput[2] = {true,true};
-int inputs[2][3] = {{0,0,0},{0,0,0}};
-int index[2] = {0,0};
+int inputs[2][3] = {{tMax,tMax,tMax},{tMax,tMax,tMax}};
+unsigned int index[2] = {0,0};
 float maxSpeed[2] = {0,0};
 
 
@@ -16,7 +17,7 @@ void updateSpeed(int bike){
    if(millis()-lastSequence[bike] > tSequence){
     index[bike]++;
     index[bike]%=lInputs;
-    inputs[bike][index[bike]] = 0;
+    inputs[bike][index[bike]] = tMax;
     lastSequence[bike] = millis();
   }
   // read the input on analog pin 0:
@@ -30,8 +31,17 @@ void updateSpeed(int bike){
     lastSequence[bike] = millis();
   }
   lastInput[bike] = sensorValue;
+}
 
-  
+void calculateSpeed(int bike){
+  float speed = 0;
+    for(int i=0;i<lInputs;++i){
+      speed[bike] += inputs[s][i];
+    }
+    speed[bike] /= (float)lInputs;
+    speed[bike] = (1000.0/speed[bike])-1;
+    if(speed[bike] > maxSpeed[bike] && speed[bike] < 10) maxSpeed[bike] = speed[bike];
+    else if (speed[bike] < 0) speed[bike] = 0;
 }
 
 void setup() {
@@ -58,13 +68,18 @@ void loop() {
     speed[s] /= (float)lInputs;
     speed[s] = (1000.0/speed[s])-1;
     if(speed[s] > maxSpeed[s] && speed[s] < 10) maxSpeed[s] = speed[s];
+    else if (speed[s] < 0) speed[s] = 0;
   }
-  Serial.print(maxSpeed[0]);
-  Serial.print(" <- ");
-  Serial.print(speed[0]);
-  Serial.print( " > < ");
+  //Serial.print(maxSpeed[0]);
+  //Serial.print(" <- ");
+  //Serial.print(speed[0]);
+  //Serial.print( " > < ");
   Serial.print(speed[1]);
   Serial.print(" -> ");
+  for (int i=0;i<3;++i){
+    Serial.print(inputs[1][i]);
+    Serial.print(" ");
+  }
   Serial.print(maxSpeed[1]);
   Serial.println();
     
