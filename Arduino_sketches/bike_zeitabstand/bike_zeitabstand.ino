c8bec896a77cb058e72066d759c9812e56cdc93b
Brian Eschrich <hi@brinoausrino.com>
2019-05-22 20:24:37 +0200

    updated send sketch

--- Arduino_sketches/bike_zeitabstand/bike_zeitabstand.ino
+++ Arduino_sketches/bike_zeitabstand/bike_zeitabstand.ino
@@ -1,8 +1,22 @@
+#include <RCSwitch.h>
+
+//rc
+RCSwitch mySwitch = RCSwitch();
+const int rcPin = 10;
+const int sendOption = 24;
+const int sendIntervall = 1000;
+long lastSend = 0;
+
+//serial
+char serialBuffer[3];
+float speedMult[2] = {1.0,1.0};
+bool fixedSpeed[2] = {false,false};
 
 //speed tracking
 const int nBikes = 2;
 const int speedPin[2] = {7,8};
 const int lInputs = 3;
+char buf[10];
 
 long lastSequence[2] = {0,0};
 int tSequence = 500;
@@ -10,7 +24,7 @@ int tMax = 1500;
 bool lastInput[2] = {true,true};
 int inputs[2][3] = {{tMax,tMax,tMax},{tMax,tMax,tMax}};
 unsigned int index[2] = {0,0};
-float maxSpeed[2] = {0,0};
+float speed[2] = {0,0};
 
 
 void updateSpeed(int bike){
@@ -34,19 +48,57 @@ void updateSpeed(int bike){
 }
 
 void calculateSpeed(int bike){
-  float speed = 0;
+  if(!fixedSpeed[bike]){
+    speed[bike] = 0;
     for(int i=0;i<lInputs;++i){
-      speed[bike] += inputs[s][i];
+      speed[bike] += inputs[bike][i];
     }
     speed[bike] /= (float)lInputs;
     speed[bike] = (1000.0/speed[bike])-1;
-    if(speed[bike] > maxSpeed[bike] && speed[bike] < 10) maxSpeed[bike] = speed[bike];
-    else if (speed[bike] < 0) speed[bike] = 0;
+    if (speed[bike] < 0) speed[bike] = 0;
+  }
 }
 
+void evaluateSerialInput(){
+  switch(serialBuffer[0]){
+    case 'm':{ //multiplicator
+      float m = (float)((int)serialBuffer[1])*0.00784;
+      if((int)serialBuffer[1] == 127) m = 1.0;
+      speedMult[(int)serialBuffer[1]] = m;
+      Serial.print("#mult ");
+      Serial.print((int)serialBuffer[1]);
+      Serial.print(" -> ");
+      Serial.println(m);
+      break;
+    }
+    case 's':{ //set speed
+      float s = (float)((int)serialBuffer[1])*0.03917647058;
+      speed[(int)serialBuffer[1]] = s;
+      Serial.print("#speed ");
+      Serial.print((int)serialBuffer[1]);
+      Serial.print(" -> ");
+      Serial.println(s);
+      break;
+    }
+    case 'e':{ //enable fixed speed
+      fixedSpeed[(int)serialBuffer[1]] = (bool)serialBuffer[2];
+      Serial.print("#fixedSpeed ");
+      Serial.print((int)serialBuffer[1]);
+      Serial.print(" -> ");
+      Serial.println((bool)serialBuffer[2]);
+      break;
+    }
+  }
+}
+
+void rcSendSpeed(){
+  mySwitch.send((int)speed[0]*2, sendOption);
+}
+
+
 void setup() {
-  // initialize serial communication at 9600 bits per second:
   Serial.begin(9600);
+  mySwitch.enableTransmit(rcPin);
 
   //init bikes
   for(int i=0; i<nBikes; ++i){
@@ -56,32 +108,34 @@ void setup() {
 
 // the loop routine runs over and over again forever:
 void loop() {
+
+  //serial input
+  while (Serial.available() > 0)
+  {
+    Serial.readBytes(serialBuffer, 3);
+    evaluateSerialInput();
+  } 
+
+  //update bikes
   for(int i=0; i<nBikes;++i){
     updateSpeed(i);
+    calculateSpeed(i);
   }
 
-  float speed[2] = {0,0};
-  for(int s=0;s<2;++s){
-    for(int i=0;i<lInputs;++i){
-      speed[s] += inputs[s][i];
-    }
-    speed[s] /= (float)lInputs;
-    speed[s] = (1000.0/speed[s])-1;
-    if(speed[s] > maxSpeed[s] && speed[s] < 10) maxSpeed[s] = speed[s];
-    else if (speed[s] < 0) speed[s] = 0;
+  if(lastSend - millis() > sendIntervall){
+    Serial.println("send");
+    Serial.print("s");
+  dtostrf(speed[0], 3, 2, buf);
+  Serial.print(buf);
+  dtostrf(speed[1], 3, 2, buf);
+  Serial.println(buf);
+    rcSendSpeed();
+    lastSend = millis();
   }
-  //Serial.print(maxSpeed[0]);
-  //Serial.print(" <- ");
-  //Serial.print(speed[0]);
-  //Serial.print( " > < ");
-  Serial.print(speed[1]);
-  Serial.print(" -> ");
-  for (int i=0;i<3;++i){
-    Serial.print(inputs[1][i]);
-    Serial.print(" ");
-  }
-  Serial.print(maxSpeed[1]);
-  Serial.println();
-    
-  delay(10);        
+  
+
+  //serial output speed
+  //
+
+ // delay(10);        
 }
